c8e092afe67e5de47bbdb7f937f485e150b0b96b
EthanHaller <ttk4ey@virginia.edu>
2023-06-06 11:36:07 -0400

    theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react"
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./Components/Navigation/Navbar"
 import Test from "./Components/Test/Test"
+import { ThemeProvider } from "@mui/material/styles"
+import theme from "./Theme"
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Test />} />
-			</Routes>
-		</BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Test />} />
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
 	)
 }
 
---
+++ frontend/src/Theme.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: '#C1D0B5',
+    },
+    secondary: {
+        main: '#D6E8DB'
+    }
+  },
+});
+
+export default theme

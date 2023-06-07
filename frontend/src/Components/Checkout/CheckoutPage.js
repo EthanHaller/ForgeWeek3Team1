a8e1402daeee33ee443e1bb502157d1b03898290
EthanHaller <ttk4ey@virginia.edu>
2023-06-07 15:24:58 -0400

    stripe is working

--- frontend/src/Components/Checkout/CheckoutPage.js
+++ frontend/src/Components/Checkout/CheckoutPage.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import { Button } from "@mui/material"
+import axios from "axios"
+import React, { useState } from "react"
+import { Navigate } from "react-router-dom"
 
 function CheckoutPage() {
-    return (
-        <>
-        </>
-    )
+
+    const handleCheckout = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:9000/checkout')
+        .then(res => window.location.href = res.data)
+    }
+
+	return (
+		<>
+			<Button onClick={handleCheckout} >TEST CHECKOUT</Button>
+		</>
+	)
 }
 
 export default CheckoutPage

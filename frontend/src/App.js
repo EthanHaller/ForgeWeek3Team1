f5f6c7ff3e88773e59122733ce872ce8ec21103f
Alex Talreja <alextalreja@gmail.com>
2023-06-07 13:13:32 -0400

    cart functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+
 import Cart from "./components/navbar/Cart";
+import TestStore from "./components/navbar/TestStore";
+import CartContext from "./components/navbar/CartContext";
+import { useState, createContext } from "react";
+
 function App() {
-	return <Cart />;
+	const [testProducts, setTestProducts] = useState([1, 2, 3, 4]);
+
+	console.log("Test Products array", testProducts);
+
+	return (
+		<CartContext.Provider value={{ testProducts, setTestProducts }}>
+			<TestStore />
+			<Cart />
+		</CartContext.Provider>
+	);
 }
 
 export default App;

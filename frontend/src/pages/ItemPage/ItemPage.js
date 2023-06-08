46fe9a2970ea42f0ba9a5996a0f2f21c1d1a8dd1
f f <nolanreedhill@gmail.com>
2023-06-08 11:00:43 -0400

    laptop work

--- frontend/src/pages/ItemPage/ItemPage.js
+++ frontend/src/pages/ItemPage/ItemPage.js
@@ -1,13 +1,16 @@
 import "./ItemPage.css"
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
-import { Button, Divider, TextField } from "@mui/material";
+import { Button, Divider, TextField, MenuItem, Box } from "@mui/material";
 import Stars from "./Stars";
 
 export default function ItemPage() {
     const [thisItem, setThisItem] = useState();
     const [otherItems, setOtherItems] = useState([]);
     const [currentBicPic, setCurrentBigPic] = useState();
+    const [quantity, setQuantity] = useState(1)
+    const [color, setColor] = useState();
+    const [size, setSize] = useState();
 
     const params = useParams();
     const id = params.id;
@@ -32,6 +35,22 @@ export default function ItemPage() {
         getItem();
 
     }, [])
+
+    useEffect(() => {
+        if (thisItem)
+            if (thisItem.item.colors) {
+                setColor(thisItem.item.colors[0])
+            }
+
+    }, [thisItem])
+
+    function handleQuantity(e) {
+        setQuantity(e.target.value);
+    }
+
+    function handleSize(e) {
+        setSize(e.target.value);
+    }
 
     return (
         <>
@@ -73,18 +92,61 @@ export default function ItemPage() {
                                         ${thisItem.item.price}
                                         <h5 style={{ color: "gray", textAlign: "left", marginTop: "0%" }}>Was <s>${Math.round(thisItem.item.price * 1.25)}</s></h5>
                                     </h3>
-
                                 </h3>
-
+                                {thisItem.item.colors &&
+                                    <div>
+                                        <h4 style={{ textAlign: "left", marginTop: "0%", textTransform: "capitalize" }}>Color: {color}</h4>
+                                        <div style={{ display: "flex" }}>
+                                            {thisItem.item.colors.map((element) =>
+                                                <Box
+                                                    className="boxes"
+                                                    sx={{
+                                                        backgroundColor: element, height: "auto",
+                                                        aspectRatio: "1", width: "10%", marginLeft: "3%",
+                                                        boxShadow: color === element ? "0 0 5px 5px #99a98F" : "none", border: color === element ? "none" : "groove"
+                                                    }}
+                                                    onClick={() => {
+                                                        setColor(element);
+                                                    }}
+                                                ></Box>
+                                            )}
+                                        </div>
+                                    </div>}
+                                {thisItem.item.sizes &&
+                                    <TextField
+                                        select label="Size:"
+                                        defaultValue={thisItem.item.sizes[0]}
+                                        sx={{ marginTop: "10%", width: "50%", textTransform: "capitalize" }}
+                                        onChange={handleSize}
+                                    >
+                                        {thisItem.item.sizes.map((element) =>
+                                            <MenuItem value={element} sx={{ textTransform: "capitalize" }}>{element}</MenuItem>
+                                        )}
+                                    </TextField>}
+                                <h3 style={{ textAlign: "left", marginBottom: "0%" }}>Description</h3>
+                                <h3 style={{ textAlign: "left", marginTop: "0%", fontWeight: "normal" }}>{thisItem.item.description}</h3>
                             </div>
                             <div className="halves" style={{ marginTop: "0%" }}>
-                                <div className="halves" style={{ border: "groove", display: "flex", flexDirection: "column" }}>
+                                <div className="halves" style={{ border: "groove", display: "flex", flexDirection: "column", height: "fit-content" }}>
                                     <h2 style={{ marginLeft: "5%", textAlign: "left" }}>${thisItem.item.price}</h2>
                                     {thisItem.item.stock > 9 ?
                                         <h3 style={{ marginLeft: "5%", textAlign: "left", color: "#1b5e20", marginTop: "0%" }}>In Stock</h3> :
                                         <h3 style={{ marginLeft: "5%", textAlign: "left", color: "red", marginTop: "0%" }}>Only {thisItem.item.stock} Left</h3>
                                     }
-                                    <TextField select sx={{ width: "fit-content" }} size="small"></TextField>
+                                    <TextField select sx={{ width: "fit-content", marginLeft: "5%" }} size="small" defaultValue="1" onChange={handleQuantity}>
+                                        <MenuItem value="1">Qty: 1</MenuItem>
+                                        <MenuItem value="2">Qty: 2</MenuItem>
+                                        <MenuItem value="3">Qty: 3</MenuItem>
+                                        <MenuItem value="4">Qty: 4</MenuItem>
+                                        <MenuItem value="5">Qty: 5</MenuItem>
+                                        <MenuItem value="6">Qty: 6</MenuItem>
+                                        <MenuItem value="7">Qty: 7</MenuItem>
+                                        <MenuItem value="8">Qty: 8</MenuItem>
+                                    </TextField>
+                                    <Button variant="contained"
+                                        sx={{ marginTop: "5%", borderRadius: "100px", width: "90%", marginLeft: "auto", marginRight: "auto" }}
+                                    >Add to Cart</Button>
+                                    <h6 style={{ textAlign: "left", marginLeft: "5%" }}>Sold by {thisItem.item.brand}</h6>
                                 </div>
                             </div>
                         </div>
